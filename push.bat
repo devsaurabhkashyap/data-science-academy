052d89385a4463bd411524309d3fda7d6d10d702
Saurabh Kashyap <saurabhkashyap2k24@gmail.com>
2026-05-27 12:08:41 +0530

    Fix Vercel build error (a11y warnings on social links)

--- push.bat
+++ push.bat
@@ -1,6 +1,3 @@
-"C:\Program Files\Git\cmd\git.exe" init
 "C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Initial commit for Binary Blooms"
-"C:\Program Files\Git\cmd\git.exe" branch -M main
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/devsaurabhkashyap/data-science-academy.git
-"C:\Program Files\Git\cmd\git.exe" push -u origin main
+"C:\Program Files\Git\cmd\git.exe" commit -m "Fix Vercel build error (a11y warnings on social links)"
+"C:\Program Files\Git\cmd\git.exe" push origin main
